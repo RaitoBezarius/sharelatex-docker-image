691a363f61e3d51c779190d09be68de002b0037d
Raito Bezarius <masterancpp@gmail.com>
2015-05-26 18:40:16 +0200

    Fix arguments in the script...

--- start_service.sh
+++ start_service.sh
@@ -3,9 +3,9 @@ docker run --name SLaTeXMongo -d mongo:2.6 --smallfiles
 docker run --name SLaTeXRedis -d redis
 docker run -d \
 	-v ~/.sharelatex_data:/var/lib/sharelatex \
-	-p $1:80 \
+	-p $2:80 \
 	--name SLaTeX \
-	--env SHARELATEX_SITE_URL="http://$0:$1" \
+	--env SHARELATEX_SITE_URL="http://$1:$2" \
 	--env SHARELATEX_APP_NAME="Yvan's ShareLaTeX" \
 	--link SLaTeXMongo:mongo \
 	--link SLaTeXRedis:redis \
